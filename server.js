912292a952eda9d9c10b8c9e471c2d1411a02c94
VincentWei <windweiss@gmail.com>
2016-07-04 00:20:46 -0700

    server.js update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require('http')
 
-http.createServer(function(request, response){
+var onRequest(request, response){
+    console.log('Request received');
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello World');
-    response.end();
-}).listen(8888)
+    response.end()
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started');
