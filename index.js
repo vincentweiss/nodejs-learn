985e0241c4d4bf456cf34c292a365ba6f166fd51
VincentWei <windweiss@gmail.com>
2016-07-04 01:07:15 -0700

    index.js updated

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Vincent Wei
+* @Date:   2016-07-04 16:06:46
+* @Last Modified by:   Vincent Wei
+* @Last Modified time: 2016-07-04 16:07:22
+*/
+
+'use strict';
+
+var server = require('./server');
+
+server.start();
