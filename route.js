7c22b2742daf95705024bca7b9fe0370eb269bd2
VincentWei <windweiss@gmail.com>
2016-07-04 01:39:34 -0700

    route.js update

---
+++ route.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Vincent Wei
+* @Date:   2016-07-04 16:38:20
+* @Last Modified by:   Vincent Wei
+* @Last Modified time: 2016-07-04 16:39:26
+*/
+
+'use strict';
+
+function route(pathname){
+    console.log('About to route a request for' + pathname);
+}
+
+exports.route = route;
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-var http = require('http')
+
+var http = require('http'),
+    url = require('url');
 
 function start(){
+
     // 回调函数
     function onRequest(request, response){
+        // 获取pathname
         var pathname = url.parse(request.url).pathname;
-        console.log('Request received');
+        console.log('Request for' + pathname + 'received.');
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('Hello World');
         response.end();
     }
 
+    // 创建服务器，监听8888端口
     http.createServer(onRequest).listen(8888);
+
     console.log('Server has started');
 
 }
